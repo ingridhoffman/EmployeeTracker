1a2383862dc86f7dcb1ae17d6a831ccf09439c1e
trapezechic <ingridhoffman@me.com>
2020-04-07 20:14:53 -0700

    build database schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  dept_id INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NULL,
+  PRIMARY KEY (id)
+);
